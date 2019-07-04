2fa3d8bb82010dc4df7f3a6a12f2999203efed5e
Bryan <bryan.dov.bergman@gmail.com>
2019-07-04 20:33:47 +0300

    Initial commit

---
+++ Variables.py
@@ -0,0 +1,34 @@
+_author_ = 'dev'
+# greeting = "bruce"
+#
+# #variable name must be letter or underscore
+# _name = "bryan"
+# Bryan45 = "good"
+# Bryan_was_35 = "hello"
+# greeting  = "there"
+#
+# print (Bryan_was_35 + ' ' + greeting)
+#
+# age = 24
+# print(age)
+# print(greeting + " "+ age)
+
+# a = 12
+# b = 3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+#
+# for i in range(1, a//b):
+#     print(i)
+
+a = 12
+b = 3
+print(a + b / 3 - 4 * 12)
+print(8 / 2 * 3)
+print(8 * 3 / 2)
+print((((a+b) / 3) -4) *12)
+
